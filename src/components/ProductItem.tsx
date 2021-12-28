d975538931f7cf7be50da3ac81c04c86a39b08e1
paulobeckman <beckmam.paulo@hotmail.com>
2021-12-28 17:56:00 -0300

    feat: memo, useMemo, useCallback

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,15 +1,23 @@
+import { memo } from 'react';
+
 interface ProductItemProps {
   product: {
     id: number;
     price: number;
     title: number;
   }
+  onAddToWishlist: (id: number) => void;
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+export function ProductItemComponent({ product, onAddToWishlist }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
+      <button onClick={() => onAddToWishlist(product.id)}>Add to wishlist</button>
     </div>
   )
 }
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.product, nextProps.product)
+});
